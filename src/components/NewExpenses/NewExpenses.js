f990267bd495311aea8ed34830f8ea7cd07fa3ff
Akshita Patel <akshitapatel@Akshitas-MacBook-Air.local>
2022-08-05 22:12:39 -0400

    Adding Expense Form Components

--- src/components/NewExpenses/NewExpenses.js
+++ src/components/NewExpenses/NewExpenses.js
@@ -1,9 +1,10 @@
 import React from "react";
+import ExpenseForm from "./ExpenseForm";
 import "./NewExpenses.css";
 const NewExpense = () => {
   return (
-    <div>
-      <form></form>
+    <div className="new-expense">
+      <ExpenseForm></ExpenseForm>
     </div>
   );
 };
